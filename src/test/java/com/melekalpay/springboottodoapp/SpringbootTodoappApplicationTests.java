0eb5922bb5831ad8e746494e539e5eb336f29605
melekalpay <melekalpy7@gmail.com>
2022-10-12 23:57:21 +0300

    h2 config

---
+++ src/test/java/com/melekalpay/springboottodoapp/SpringbootTodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melekalpay.springboottodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
