6543e988c97c2c6212826b61948c3d1d1570aea7
melekalpay <melekalpy7@gmail.com>
2022-10-13 00:23:28 +0300

    models done

---
+++ src/main/java/com/melekalpay/springboottodoapp/Models/Todoitem.java
@@ -0,0 +1,60 @@
+package com.melekalpay.springboottodoapp.Models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.Instant;
+
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "todo_item")
+public class Todoitem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
+    private Long id;
+
+    @Getter
+    @Setter
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @Getter
+    @Setter
+    private boolean complete;
+
+    @Getter
+    @Setter
+    private Instant createdDate;
+
+    @Getter
+    @Setter
+    private Instant modifiedDate;
+
+    public Todoitem() {}
+
+    public Todoitem(String description) {
+        this.description = description;
+        this.complete = false;
+        this.createdDate = Instant.now();
+        this.modifiedDate = Instant.now();
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("TodoItem{id=%d, description='%s', complete='%s', createdDate='%s', modifiedDate='%s'}",
+        id, description, complete, createdDate, modifiedDate);
+    }
+
+    
+}
